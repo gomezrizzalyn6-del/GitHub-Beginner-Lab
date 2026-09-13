a30e4368d07a8782b6bcba5344db79019f89f962
Gomez, Rizzalyn (Student) <gomez.394276@balayan.sti.edu.ph>
2026-09-13 14:56:51 +0800

    Implement student profile layout (#1)

--- GitHub-Beginner-Lab/Form1.cs
+++ GitHub-Beginner-Lab/Form1.cs
@@ -16,5 +16,15 @@ namespace GitHub_Beginner_Lab
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtyearlevel_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
